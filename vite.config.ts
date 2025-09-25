9b24b2f15b2ab57e5c7a5d43df42b571aac705ac
格子 <fl9420@qq.com>
2025-09-25 20:50:03 +0800

    init: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig(async () => ({
+    plugins: [
+        vue(),
+        Components({
+            dts: 'src/components.d.ts',
+            resolvers: [
+                AntDesignVueResolver({
+                    importStyle: false,
+                }),
+            ],
+        }),
+    ],
+    clearScreen: false,
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            ignored: ["**/src-tauri/**"],
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+}));
