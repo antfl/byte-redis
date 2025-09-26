b5a960a5168debeea3c00136671bb2c5f95096fc
格子 <fl9420@qq.com>
2025-09-26 22:36:53 +0800

    chore: 项目配置相关

---
+++ uno.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, presetAttributify, transformerDirectives, presetWind3 } from 'unocss';
+
+export default defineConfig({
+  content: {
+    pipeline: {
+      include: [/\.(vue|ts|js|tsx|jsx)$/],
+      exclude: ['node_modules/**', 'dist/**', '.git/**', '**/*.md'],
+    },
+  },
+  presets: [presetWind3(), presetAttributify()],
+  transformers: [transformerDirectives()],
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,44 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from 'path';
+import UnoCSS from 'unocss/vite'
 import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
+
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-    plugins: [
-        vue(),
-        Components({
-            dts: 'src/components.d.ts',
-            resolvers: [
-                AntDesignVueResolver({
-                    importStyle: false,
-                }),
-            ],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    Components({
+      dts: 'src/types/components.d.ts',
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false,
         }),
-    ],
-    clearScreen: false,
-    server: {
-        port: 1420,
-        strictPort: true,
-        host: host || false,
-        hmr: host
-            ? {
-                protocol: "ws",
-                host,
-                port: 1421,
-            }
-            : undefined,
-        watch: {
-            ignored: ["**/src-tauri/**"],
-        },
+      ],
+    }),
+  ],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
 }));
